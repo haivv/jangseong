6c9d514523296dfdeaab2648ea39ad62ca055f77
Van-Hai Vu <50228260+haivv@users.noreply.github.com>
2023-05-12 15:09:01 +0900

    Add files via upload

--- jangseong/jangseong.sql
+++ jangseong/jangseong.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2023 at 02:43 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.1.17
+-- Generation Time: May 12, 2023 at 08:08 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,50 +24,180 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `accID` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `authority` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`accID`, `username`, `password`, `authority`) VALUES
+(1, 'member1', '123', 1),
+(2, 'member2', '123', 2),
+(3, 'member3', '123', 3),
+(4, 'member4', '123', 4),
+(5, 'member5', '123', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authority`
+--
+
+CREATE TABLE `authority` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `authority`
+--
+
+INSERT INTO `authority` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, '휠형'),
+(3, '궤도형'),
+(4, '도저'),
+(5, '로더'),
+(6, '그레이더');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `member`
 --
 
 CREATE TABLE `member` (
   `id` int(11) NOT NULL,
-  `num` text NOT NULL,
-  `date` text NOT NULL,
-  `memID` text NOT NULL,
-  `name` text NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `nofpass` int(11) NOT NULL,
-  `noftest` int(11) NOT NULL
+  `num` varchar(10) NOT NULL,
+  `date` date NOT NULL,
+  `memID` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`id`, `num`, `date`, `memID`, `name`, `password`, `nofpass`, `noftest`) VALUES
-(30, '23-2', '2023-04-17', '23-000003', '정재혁', '', 0, 0),
-(31, '23-2', '2023-04-17', '23-000001', '김재순', '1234', 0, 0),
-(32, '23-2', '2023-04-17', '23-000002', '김신우', '1234', 0, 0),
-(33, '22-1', '2022-01-17', '22-000001', '황희철', '1234', 0, 0),
-(34, '24-1', '2024-01-17', '24-000002', '권정인', '1234', 0, 0);
+INSERT INTO `member` (`id`, `num`, `date`, `memID`, `name`, `password`) VALUES
+(1, '23-2', '2023-04-17', '23-000001', '정재혁', ''),
+(2, '23-2', '2023-04-17', '23-000002', '김신우', ''),
+(3, '23-2', '2023-04-17', '23-000003', '황희철', ''),
+(4, '23-1', '2024-01-17', '24-000002', '권정인', ''),
+(52, '22-3', '2023-10-14', '23-123145', '김원수', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `record`
+--
+
+CREATE TABLE `record` (
+  `id` int(11) NOT NULL,
+  `memID` varchar(20) NOT NULL,
+  `result` varchar(10) NOT NULL,
+  `time` time NOT NULL,
+  `equipment` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `record`
+--
+
+INSERT INTO `record` (`id`, `memID`, `result`, `time`, `equipment`) VALUES
+(1, '23-000001', '합격', '00:03:30', '휠형 굴착기'),
+(2, '23-000001', '합격', '00:03:30', '휠형 굴착기'),
+(3, '23-000001', '합격', '00:03:30', '휠형 굴착기'),
+(4, '23-000001', '합격', '00:03:30', '휠형 굴착기'),
+(5, '23-000001', '불합격', '00:03:30', '휠형 굴착기'),
+(6, '23-000002', '합격', '00:03:30', '로더'),
+(7, '23-000001', '합격', '00:03:30', '휠형 굴착기'),
+(8, '23-000001', '합격', '00:03:30', '휠형 굴착기'),
+(9, '23-000002', '합격', '00:03:30', '휠형 굴착기'),
+(12, '23-000001', '합격', '00:03:30', '휠형 굴착기');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`accID`),
+  ADD KEY `Authority` (`authority`);
+
+--
+-- Indexes for table `authority`
+--
+ALTER TABLE `authority`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `member`
 --
 ALTER TABLE `member`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `memID` (`memID`);
+
+--
+-- Indexes for table `record`
+--
+ALTER TABLE `record`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ReTest` (`memID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `accID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `authority`
+--
+ALTER TABLE `authority`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `record`
+--
+ALTER TABLE `record`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `Authority` FOREIGN KEY (`authority`) REFERENCES `authority` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `record`
+--
+ALTER TABLE `record`
+  ADD CONSTRAINT `ReTest` FOREIGN KEY (`memID`) REFERENCES `member` (`memID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
